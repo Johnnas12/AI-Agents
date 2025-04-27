4c3273cf7219fa1a0bc2be28db75eca2dbb6f8e0
johnnas12 <yohanesmesfin3@gmail.com>
2025-04-27 09:22:47 +0300

    Feat: Reasercher Agents complete with its test

--- agents/researcher_agent.py
+++ agents/researcher_agent.py
@@ -1,16 +1,20 @@
 from agent import Agent
-
+import google.generativeai as genai
 
 class ResearcherAgent(Agent):
     def run(self, topic):
         print(f"[{self.name}], Researching topic: {topic}") 
-
-        # Simple simulation of research
         research_results = self.perform_research(topic)
         return research_results
 
         
     def perform_research(self, topic):
-        # mock research results to be returned
-        mock_results =  f"Gathered research on {topic}: \n" 
-        return mock_results
+        # This is where the actual research would be performed.
+        genai.configure(api_key=self.api_key)
+        model = genai.GenerativeModel(self.model)
+
+        prompt = f"Research and provide a concise technical overview about: {topic}"
+        response = model.generate_content(prompt)
+        research_text = response.candidates[0].content.parts[0].text
+
+        return research_text
